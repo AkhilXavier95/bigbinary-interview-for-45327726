4bbf94634aef05dea122e18fe337de946116e80b
Akhil xavier <akhilxavier95@gmail.com>
2021-02-26 16:58:08 +0530

    launchfilter hidden - pagination fix

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -10,18 +10,22 @@ const PaginationComponent = ({
   onChange,
 }) => {
   return (
-    <div className="d-flex justify-content-end pagination">
-      <Pagination
-        itemClass="page-item"
-        linkClass="page-link"
-        activePage={activePage}
-        itemsCountPerPage={countPerPage}
-        totalItemsCount={totalCount}
-        pageRangeDisplayed={5}
-        onChange={onChange}
-        hideFirstLastPages
-      />
-    </div>
+    <>
+      {totalCount > 0 && (
+        <div className="d-flex justify-content-end pagination">
+          <Pagination
+            itemClass="page-item"
+            linkClass="page-link"
+            activePage={activePage}
+            itemsCountPerPage={countPerPage}
+            totalItemsCount={totalCount}
+            pageRangeDisplayed={5}
+            onChange={onChange}
+            hideFirstLastPages
+          />
+        </div>
+      )}
+    </>
   );
 };
 
